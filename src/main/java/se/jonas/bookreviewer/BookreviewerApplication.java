78e6c86ab3dcdcb0e531f115641bf281beec1375
“[Jonas <joner.svensson@gmail.com>
2026-09-21 09:54:43 +0200

    Spring Boot init

---
+++ src/main/java/se/jonas/bookreviewer/BookreviewerApplication.java
@@ -0,0 +1,13 @@
+package se.jonas.bookreviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreviewerApplication.class, args);
+	}
+
+}
